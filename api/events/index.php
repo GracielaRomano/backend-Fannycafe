32c63b67df469b70c07596b0f2c9e2755e340acd
Graciela Romano <graci.romano@gmail.com>
2025-10-28 14:00:37 -0300

    Modificacion en archivos show

--- api/events/index.php
+++ api/events/index.php
@@ -6,11 +6,6 @@
  * URL: http://localhost:8000/backend/api/events/
  */
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once __DIR__ . '/../../config/database.php';
 include_once __DIR__ . '/../../models/Event.php';
